4d8c3d3894ef7074c2411da662128ca3f9695acf
bndnt <l.makushchenko@gmail.com>
2020-12-20 22:36:34 +0200

    еще чутка комментов

--- 1.js
+++ 1.js
@@ -8,14 +8,14 @@ let fish=["Судак"];//1
  for(i=0;i<5;i++){
     console.log(fish[i]);
  }
- // fish.sort();
- // for(i=0;i<5;i++){
- //    console.log(fish[i]);
- // }
-// fish.reverse();
-//  for(i=0;i<5;i++){
-//     console.log(fish[i]);
-//  }
+ /*fish.sort();
+  for(i=0;i<5;i++){
+     console.log(fish[i]);
+  }*/
+/* fish.reverse();
+  for(i=0;i<5;i++){
+    console.log(fish[i]);
+  }*/
 
 fish.splice(1,0,"Щука","Плотва");//6
  for(i=0;i<7;i++){
@@ -25,23 +25,23 @@ fish.splice(6,0,"Язь","Пескарь");//7
  for(let i=0;i<9;i++){
     console.log(fish[i]);
   }
-// fish.shift();
-// for(let i=0;i<8;i++){
-//     console.log(fish[i]);
-// }
-// fish.pop();
-// for(let i=0;i<7;i++){
-//     console.log(fish[i]);
-// }
-// fish.splice(2,1);
-// for(let i=0;i<6;i++){
-//     console.log(fish[i]);
-// }
+/* fish.shift();
+ for(let i=0;i<8;i++){
+     console.log(fish[i]);
+ }*/
+/* fish.pop();
+ for(let i=0;i<7;i++){
+     console.log(fish[i]);
+}*/
+/* fish.splice(2,1);
+ for(let i=0;i<6;i++){
+     console.log(fish[i]);
+ }*/
 let i=0;//11
 while( i<3){
     console.log(fish[i]);
     i++;
 }
-// for(let i=0;i<3;i++){
-//     console.log(fish[i]);
-// }
+/* for(let i=0;i<3;i++){
+    console.log(fish[i]);
+ }*/
